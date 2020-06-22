bd2b3743cefa4ee8380a8065d41909bb10d7e14f
DanBi-Lee <danbi.s.rain@gmail.com>
2020-06-23 01:12:43 +0900

    Update 2020-06-22T16:12:31.122Z

--- js/mkimage.js
+++ js/mkimage.js
@@ -23,3 +23,77 @@ $('.download').on('click', function(){
     return console.error(error);
   });
 });
+
+var $btnList = $mkimageBox.find('.step_list > li');
+var $optionBox = $mkimageBox.find('.option_box > li');
+
+$btnList.on('click', function(e){
+  e.preventDefault();
+
+  var $this = $(this);
+  var index = $this.index();
+
+  $optionBox.removeClass('on')
+  $optionBox.eq(index).addClass('on');
+
+  $btnList.removeClass('on');
+  $this.addClass('on');
+});
+
+(function(){
+  var imageData = {
+    text : {
+      main : '메인 텍스트 예시입니다',
+      sub : '서브 텍스트 예시입니다'
+    },
+    img : 'https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+    font : {
+      main : 'CookieRun-Regular',
+      sub : 'Godo'
+    },
+    filter : {
+      grayscale : 0,
+      blur : 0,
+      invert : 0,
+      contrast: 1,
+      sepia : 0
+    },
+    filterBox : {
+      opacity : 0.3,
+      color : '#ffffff'
+    }
+  };
+
+  var inputDom = {
+    text : {
+      main : '#inputMainText',
+      sub : '#inputSubText'
+    }
+  };
+
+  var outputDom = {
+    text : {
+      main : '.title',
+      sub : '.desc'
+    }
+  }
+
+  function bindingEvent(){
+    changeText('main');
+    changeText('sub');
+  }
+
+  bindingEvent();
+
+  function changeText(data){
+    $(inputDom['text'][data]).on('focusout', function(){
+      imageData['text'][data] = $(this).attr('value');
+      $('#mkimageBox .card').find(outputDom['text'][data]).text(imageData['text'][data]);
+      render(card);
+    });
+  }
+
+
+
+})();
+
