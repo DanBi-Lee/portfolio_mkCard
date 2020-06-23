42676c8a0e91522d150488077f3962dd23b0edf8
DanBi-Lee <danbi.s.rain@gmail.com>
2020-06-23 19:41:21 +0900

    Update 2020-06-23T10:41:10.136Z

--- js/mkimage.js
+++ js/mkimage.js
@@ -26,27 +26,36 @@ $('.download').on('click', function(){
 
 var $btnList = $mkimageBox.find('.step_list > li');
 var $optionBox = $mkimageBox.find('.option_box > li');
+Tabmenu($btnList, $optionBox);
 
-$btnList.on('click', function(e){
-  e.preventDefault();
+var $fontBox = $mkimageBox.find('.font_box');
+var $btnFontList = $fontBox.find('.header_font > li');
+var $boxFontList = $fontBox.find('.content_font > li');
+Tabmenu($btnFontList, $boxFontList);
 
-  var $this = $(this);
-  var index = $this.index();
-
-  $optionBox.removeClass('on')
-  $optionBox.eq(index).addClass('on');
-
-  $btnList.removeClass('on');
-  $this.addClass('on');
-});
+function Tabmenu(button, box){
+    button.on('click', function(e){
+      e.preventDefault();
+    
+      var $this = $(this);
+      var index = $this.index();
+    
+      box.removeClass('on')
+      box.eq(index).addClass('on');
+    
+      button.removeClass('on');
+      $this.addClass('on');
+    });
+}
 
 (function(){
+
   var imageData = {
     text : {
-      main : '메인 텍스트 예시입니다',
-      sub : '서브 텍스트 예시입니다'
+      main : '',
+      sub : ''
     },
-    img : 'https://images.unsplash.com/photo-1434030216411-0b793f4b4173?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=900&q=60',
+    img : '',
     font : {
       main : 'CookieRun-Regular',
       sub : 'Godo'
@@ -68,6 +77,14 @@ $btnList.on('click', function(e){
     text : {
       main : '#inputMainText',
       sub : '#inputSubText'
+    },
+    img : {
+      upload : '#uploadImgfile',
+      search : $('#searchImg')
+    },
+    font : {
+        main : $('.main'),
+        sub : $('.sub')
     }
   };
 
@@ -75,25 +92,168 @@ $btnList.on('click', function(e){
     text : {
       main : '.title',
       sub : '.desc'
+    },
+    img : {
+        upload : '.card_img'
+    },
+    font : {
+        main : '.title',
+        sub : '.desc'
     }
   }
 
   function bindingEvent(){
     changeText('main');
     changeText('sub');
+    changeImg();
+    mkFontDom();
   }
 
   bindingEvent();
 
   function changeText(data){
     $(inputDom['text'][data]).on('focusout', function(){
-      imageData['text'][data] = $(this).attr('value');
-      $('#mkimageBox .card').find(outputDom['text'][data]).text(imageData['text'][data]);
-      render(card);
+    imageData['text'][data] = $(this).attr('value');
+    //   $('#mkimageBox .card').find(outputDom['text'][data]).text(imageData['text'][data]);
+    mkDom();
+    render(card);
     });
   }
 
+  function changeImg(){
+    uploadFile('upload');
+    searchImg('search');
+  }
 
+  function uploadFile(data){
+    $(inputDom['img'][data]).on('change', function(e){
+        var file = e.target.files[0];
+        if(!file.type.match(/image.*/)){
+            alert("이미지 파일만 올려주세요");
+            return;
+        }else{
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                imageData.img = e.target.result;
+                // $(outputDom['img'][data]).css({
+                //     backgroundImage : 'url(' + imageData.img + ')'
+                // });
+                mkDom();
+                render(card);
+            };
+            reader.readAsDataURL(file);
+        }
+    });
+}
+
+function searchImg(data){
+    inputDom['img'][data].siblings('button').on('click',function(e){
+        e.preventDefault();
+        console.log($(this));
+        var _key = '17163984-867a8b6f54b9fd842905aca26';
+        var _keyword = inputDom['img'][data].attr('value');
+        var settings = {
+            "url": "https://pixabay.com/api/?key=" + _key + "&q=" + _keyword + "&per_page=33",
+            "method": "GET",
+            "timeout": 0,
+            "dataType" : "jsonp"
+          };
+          $.ajax(settings).done(function (response) {
+            console.log(settings.url);
+            if(response.total == 0){
+                $('.contents_imgbox').find('p').text( _keyword + '에 대한 검색결과가 없습니다.');
+            }else{
+                $('.contents_imgbox').html('<ul>');
+                $(response.hits).each(function(index, item){
+                    var prev_img = item.previewURL;
+                    var data_img = item.largeImageURL;
+                    $('.contents_imgbox > ul').append(
+                        $('<li>')
+                            .append(
+                                $('<button>')
+                                    .css({
+                                    backgroundImage : 'url(' + prev_img + ')'
+                                    })
+                                    .addClass('search_item')
+                                    .attr('data-img', data_img)
+                            )
+                    );
+                });
+            }
+          });
+    });
+    selectImg();
+}
+
+function selectImg(){
+    $('.contents_imgbox').on('click','.search_item', function(e){
+        e.preventDefault();
+        imageData.img = $(this).attr('data-img');
+        console.log($(this));
+        // $('.card_img').css({
+        //     backgroundImage : 'url(' + imageData.img + ')'
+        // });
+        mkDom();
+        render(card);
+    });
+}
+
+function mkDom(){
+    // 텍스트
+    function text(data){
+        $('#mkimageBox .card').find(outputDom['text'][data]).text(imageData['text'][data]);
+    }
+    text('main');
+    text('sub');
+
+    // 배경이미지
+    function img(){
+        $('.card_img').css({
+            backgroundImage : 'url(' + imageData.img + ')'
+        });
+    }
+    img();
+
+    // 폰트
+    function font(data){
+      $('#mkimageBox .card').find(outputDom['font'][data]).css('font-family', imageData['font'][data]);
+    }
+
+    font('main');
+    font('sub');
+}
+
+function mkFontDom(){
+  $(fontList.fontlist).each(function(index, item){
+    var fontFamily = item.code;
+    var fontName = item.name;
+    $('.content_font').find('ul')
+      .append(
+        $('<li>')
+          .addClass('items')
+          .append(
+            $('<button>')
+              .text(fontName)
+          )
+          .attr('data-font', fontFamily)
+          .css('font-family', fontFamily)
+      );
+  });
+
+  changeFont('main');
+  changeFont('sub');
+}
+
+function changeFont(data){
+    console.log(inputDom['font'][data]);
+    inputDom['font'][data].on('click', '.font_list > li' , function(e){
+        e.preventDefault();
+        imageData['font'][data] = $(this).attr('data-font');
+        console.log(imageData['font'][data]);
+        mkDom();
+        render(card);
+      });
+}
 
 })();
 
