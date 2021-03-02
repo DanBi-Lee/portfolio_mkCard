e62d6c04a32f0a11cb1f6c5cd0a53d243aac7d81
DanBi-Lee <danbi.s.rain@gmail.com>
2021-03-02 12:50:21 +0900

    바벨 추가

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -16,40 +16,33 @@ const routes = {
   html: {
     watch: "src/**/*.html",
     src: "src/*.html",
-    dest: "build"
+    dest: "build",
   },
   img: {
     src: "src/img/**/*",
-    dest: "build/img"
+    dest: "build/img",
   },
   scss: {
     watch: "src/scss/**/*.scss",
     src: "src/scss/*.scss",
-    dest: "build/css"
+    dest: "build/css",
   },
   js: {
     watch: "src/js/**/*.js",
     src: "src/js/**/*.js",
-    dest: "build/js"
-  }
+    dest: "build/js",
+  },
 };
 
 const html = () =>
-  gulp
-    .src(routes.html.src)
-    .pipe(htmlTag())
-    .pipe(gulp.dest(routes.html.dest));
+  gulp.src(routes.html.src).pipe(htmlTag()).pipe(gulp.dest(routes.html.dest));
 
-
-const clean = () => del(["build/", ".publish"]); //build를 위한 준비과정
+const clean = () => del(["build/", ".publish"]);
 
 const webserver = () => gulp.src("build").pipe(ws({ livereload: true }));
 
 const img = () =>
-  gulp
-    .src(routes.img.src)
-    .pipe(image())
-    .pipe(gulp.dest(routes.img.dest));
+  gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 
 const styles = () =>
   gulp
@@ -59,15 +52,18 @@ const styles = () =>
     .pipe(miniCSS())
     .pipe(gulp.dest(routes.scss.dest));
 
-const js = () => 
-  gulp.src(routes.js.src)
-  // .pipe(bro({
-  //   transform: [
-  //     babelify.configure({ presets: ['@babel/preset-env'] }),
-  //     [ 'uglifyify', { global: true } ]
-  //   ]
-  // }))
-  .pipe(gulp.dest(routes.js.dest));
+const js = () =>
+  gulp
+    .src(routes.js.src)
+    .pipe(
+      bro({
+        transform: [
+          babelify.configure({ presets: ["@babel/preset-env"] }),
+          ["uglifyify", { global: true }],
+        ],
+      })
+    )
+    .pipe(gulp.dest(routes.js.dest));
 
 const gh = () => gulp.src("build/**/*").pipe(ghPages());
 
