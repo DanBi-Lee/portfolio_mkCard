7bf00a065ff6da1fbeefd60508ab31f378e6181f
DanBi-Lee <danbi.s.rain@gmail.com>
2021-03-03 11:13:34 +0900

    [#16] 메인 페이지 : 마우스 휠 모션
    Resolves #16

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,5 @@
 import MyParallex from "./myParallex";
+import WheelMotion from "./wheelMotion";
 
-const myParallex = new MyParallex('[id$="Box"]', 300);
-
-// $boxList.on('mousewheel', function(event, delta){
-//     event.preventDefault();
-
-//     var _this = $(this);
-//     var index = _this.index();
-
-//     if(delta > 0 && index !== 0 ){
-//         var prev_pos = $(this).prev().offset().top;
-//         $('html, body').stop().animate({scrollTop: prev_pos}, 1000, "easeOutBack");
-//     }else if(delta <= 0 && index !== $boxList.length-1 ){
-//         var next_pos = $(this).next().offset().top;
-//         $('html, body').stop().animate({scrollTop: next_pos}, 1000, "easeOutBack");
-//     }
-// });
+const myParallex = new MyParallex('[id$="Box"]', 250);
+const wheelMotion = new WheelMotion('[id$="Box"]');
---
+++ src/js/wheelMotion.js
@@ -0,0 +1,42 @@
+class WheelMotion {
+  $body = document.querySelector("body");
+  MIN_SIZE = 900;
+  //   필요 util함수 : PC, 모바일 구분
+
+  constructor(selector) {
+    this.selector = selector;
+    this.boxList = [...document.querySelectorAll(selector)];
+    this.init();
+  }
+
+  init = () => {
+    this.onMouseWheel();
+  };
+
+  onMouseWheel = () => {
+    this.$body.addEventListener("wheel", this.motion, { passive: false });
+  };
+
+  motionCondition = (e) => {
+    const isPC = window.innerWidth >= this.MIN_SIZE;
+    const isOnSelector = !!e.target.closest(this.selector);
+    return isPC && isOnSelector;
+  };
+
+  motion = (e) => {
+    const condition = this.motionCondition(e);
+    if (!condition) {
+      return;
+    }
+    e.preventDefault();
+    console.log(e);
+    const index = this.boxList.indexOf(e.target.closest(this.selector));
+    if (e.deltaY > 0 && index !== 0 && index !== this.boxList.length - 1) {
+      this.boxList[index + 1].scrollIntoView({ behavior: "smooth" });
+    } else if (e.deltaY < 0 && index !== 0) {
+      this.boxList[index - 1].scrollIntoView({ behavior: "smooth" });
+    }
+  };
+}
+
+export default WheelMotion;
