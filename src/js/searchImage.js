c0bbc131cbc18d95c809e60305ddd2baced3b963
DanBi-Lee <danbi.s.rain@gmail.com>
2021-03-05 01:52:39 +0900

    [#32] 서브 페이지 : 이미지 검색 결과창 무한 스크롤링 적용
    Resolves #32

--- src/js/searchImage.js
+++ src/js/searchImage.js
@@ -8,20 +8,50 @@ class SearchImage {
     this.init();
   }
 
+  index = 1;
+
   init = () => {
     this.onSearch();
   };
 
-  onSearch = () => {
-    this.$searchForm.addEventListener("submit", this.getImage);
+  observer = new IntersectionObserver(
+    (entries) => this.handlingIntersect(entries),
+    {
+      root: this.$contentsBox,
+      rootMargins: "0px",
+      threshold: 0.5,
+    }
+  );
+
+  handlingIntersect = (entries) => {
+    if (entries[0].isIntersecting) {
+      if (this.index >= this.maxIndex) {
+        this.observer.disconnect();
+        return;
+      }
+      this.index++;
+      this.$sentinel.remove();
+      this.observer.disconnect();
+      this.getImage();
+    }
   };
 
-  getImage = async (e) => {
+  onSearch = () => {
+    this.$searchForm.addEventListener("submit", this.handlingSearch);
+  };
+
+  handlingSearch = (e) => {
     e.preventDefault();
+    this.index = 1;
+    this.$contentsBox.scrollTop = 0;
     this.keyword = e.target.querySelector("#searchImg").value;
-    const data = await fetchImg(this.keyword);
+    this.getImage();
+  };
+
+  getImage = async () => {
+    const data = await fetchImg(this.keyword, this.index);
+    this.maxIndex = data.total / 36;
     const resultImage = data.hits;
-    console.log(resultImage);
     this.render(resultImage);
   };
 
@@ -29,27 +59,34 @@ class SearchImage {
     if (!data) {
       return;
     }
-    if (data.length === 0) {
+    if (data.length === 0 && this.index === 1) {
       this.$contentsBox.innerHTML = `
             <p>
                 ${this.keyword}에 대한 검색 결과가 없습니다.
             </p>
         `;
     } else {
-      this.$contentsBox.innerHTML = `
-        <ul class="result_bg"></ul>
-      `;
-      const $ul = document.querySelector(".result_bg");
-      $ul.innerHTML = data
-        .map((item) => {
-          return `
-                <li>
-                    <button style="background-image: url(${item.previewURL})" data-img="${item.largeImageURL}" class="search_item">
-                    </button>
-                </li>
-            `;
-        })
-        .join("");
+      if (this.index === 1) {
+        this.$contentsBox.innerHTML = "";
+        this.$ul = document.createElement("ul");
+        this.$ul.setAttribute("class", "result_bg");
+        this.$contentsBox.appendChild(this.$ul);
+      }
+      data.forEach((item) => {
+        const $li = document.createElement("li");
+        $li.innerHTML = `
+          <button style="background-image: url(${item.previewURL})" data-img="${item.largeImageURL}" class="search_item">
+          </button>
+        `;
+        this.$ul.appendChild($li);
+      });
+
+      this.$sentinel = document.createElement("div");
+      this.$sentinel.setAttribute("id", "sentinel");
+      this.$lastItem = this.$ul.children[this.$ul.children.length - 1];
+      this.$lastItem.appendChild(this.$sentinel);
+
+      this.observer.observe(this.$sentinel);
     }
   };
 }
